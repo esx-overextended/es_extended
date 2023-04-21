5eef069f6fd02af45f2cd01b883f6cf4dcbfb778
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-04-21 18:57:45 -0400

    upload
    take es_extended out of esx-framework/esx_core repository

--- [core]/cron/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'adamant'
-
-game 'gta5'
-author 'ESX-Framework'
-description 'cron'
-lua54 'yes'
-version '1.9.4'
-
-server_script 'server/main.lua'
--- [core]/esx_skin/locales/fi.lua
+++
@@ -1,6 +0,0 @@
-Locales['fi'] = {
-  ['skin_menu'] = 'ulkonäkö',
-  ['use_rotate_view'] = 'paina ~INPUT_FRONTEND_LS~ tai ~INPUT_CHARACTER_WHEEL~ liikutaaksesi kameraa.',
-  ['skin'] = 'vaihda ulkonäköä',
-  ['saveskin'] = 'talleta ulkonäkö tiedostoon',
-}
