03c3b70ed32be762ac8c93c2160528e2ada98286
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-06-23 23:20:25 -0400

    feat(modules/override): revert overrided xPlayer methods to OG references
    In case the resource that overrides player functions stops

--- modules/override/server.lua
+++ modules/override/server.lua
@@ -4,26 +4,33 @@ if ESX.RegisterPlayerMethodOverrides then return end
 local invokedResources = {}
 
 ---@type table<string, function>
+local originalMethods = lib.require("server.classes.playerMethods")
+
+---@type table<string, function>
 Core.PlayerMethodOverrides = {}
 
-function ESX.RegisterPlayerMethodOverrides(overrides)
-    local overridesType = type(overrides)
-    if overridesType ~= "table" then
+---Overrides player methods if they exist. If those method don't exist, it will add the new received methods to the all of the player objects(basically extends xPlayer objects methods)
+---@param newMethods table<string, function>
+function ESX.RegisterPlayerMethodOverrides(newMethods)
+    local newMethodsType = type(newMethods)
+    if newMethodsType ~= "table" then
         return print(("[^1ERROR^7] Expected a parameter with type of ^3table^7 in ^4ESX.RegisterPlayerMethodOverrides^7 function. Received (^3%s^7)"):format(overridesType))
     end
 
     local invokingResource = GetInvokingResource() or cache.resource
 
-    for fnName, fn in pairs(overrides) do
+    for fnName, fn in pairs(newMethods) do
         if Core.PlayerMethodOverrides[fnName] then
             print(("[^3WARNING^7] xPlayer method ^2'%s'^7 is already overrided by ^4'%s'^7. Re-overriding it from ^4'%s'^7..."):format(fnName, invokedResources[fnName], invokingResource))
         end
 
         Core.PlayerMethodOverrides[fnName] = fn
         invokedResources[fnName] = invokingResource
-    end
 
-    --TODO: override online players methods
+        for playerId, xPlayer in pairs(ESX.Players) do
+            ESX.Players[playerId][fnName] = fn(xPlayer) -- overriding online players methods to the new one(s)...
+        end
+    end
 end
 
 local function onResourceStop(resource)
@@ -33,7 +40,9 @@ local function onResourceStop(resource)
             Core.PlayerMethodOverrides[fnName] = nil
             invokedResources[fnName] = nil
 
-            --TODO: remove overrided online players methods
+            for playerId, xPlayer in pairs(ESX.Players) do
+                ESX.Players[playerId][fnName] = originalMethods[fnName] and originalMethods[fnName](xPlayer) or nil -- overriding online players methods to the original one(s) if exist...
+            end
         end
     end
 end
