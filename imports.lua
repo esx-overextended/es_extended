8adb6116716a55c4767e1a0042d35ddd8bb04c22
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-06-26 03:12:23 -0400

    feat(imports): automatic reload of ESX on esx:sharedObjectUpdated event

--- imports.lua
+++ imports.lua
@@ -19,54 +19,64 @@ if not IsDuplicityVersion() then -- Only register these for the client
         ESX.PlayerLoaded = false
         ESX.PlayerData = {}
     end)
+
+    AddEventHandler("esx:sharedObjectUpdated", function()
+        ESX = exports["es_extended"]:getSharedObject()
+
+        collectgarbage("collect")
+    end)
 else -- Only register these for the server
-    local _GetPlayerFromId = ESX.GetPlayerFromId
+    local function setupESX()
+        local _GetPlayerFromId = ESX.GetPlayerFromId
 
-    ESX.Jobs = setmetatable({}, {
-        __index = function(self, index)
-            if not next(self) then self() end
-            return rawget(self, index)
-        end,
-        __call = function(self)
-            for key, data in pairs(ESX.GetJobs()) do
-                rawset(self, key, data)
-            end
-            return self
-        end,
-        __pairs = function(self)
-            if not next(self) then self() end
-            return next, self, nil ---@diagnostic disable-line: redundant-return-value
-        end
-    })
-
-    ESX.Groups = setmetatable({}, {
-        __index = function(self, index)
-            if not next(self) then self() end
-            return rawget(self, index)
-        end,
-        __call = function(self)
-            for key, data in pairs(ESX.GetGroups()) do
-                rawset(self, key, data)
-            end
-            return self
-        end,
-        __pairs = function(self)
-            if not next(self) then self() end
-            return next, self, nil ---@diagnostic disable-line: redundant-return-value
-        end
-    })
-
-    function ESX.GetPlayerFromId(playerId) ---@diagnostic disable-line: duplicate-set-field
-        local xPlayer = _GetPlayerFromId(playerId)
-
-        return xPlayer and setmetatable(xPlayer, {
+        ESX.Jobs = setmetatable({}, {
             __index = function(self, index)
-                if index == "coords" then return self.getCoords() end
-
+                if not next(self) then self() end
                 return rawget(self, index)
+            end,
+            __call = function(self)
+                for key, data in pairs(ESX.GetJobs()) do
+                    rawset(self, key, data)
+                end
+                return self
+            end,
+            __pairs = function(self)
+                if not next(self) then self() end
+                return next, self, nil ---@diagnostic disable-line: redundant-return-value
             end
         })
+
+        ESX.Groups = setmetatable({}, {
+            __index = function(self, index)
+                if not next(self) then self() end
+                return rawget(self, index)
+            end,
+            __call = function(self)
+                for key, data in pairs(ESX.GetGroups()) do
+                    rawset(self, key, data)
+                end
+                return self
+            end,
+            __pairs = function(self)
+                if not next(self) then self() end
+                return next, self, nil ---@diagnostic disable-line: redundant-return-value
+            end
+        })
+
+        function ESX.GetPlayerFromId(playerId) ---@diagnostic disable-line: duplicate-set-field
+            local xPlayer = _GetPlayerFromId(playerId)
+
+            return xPlayer and setmetatable(xPlayer, {
+                __index = function(self, index)
+                    if index == "coords" then return self.getCoords() end
+
+                    return rawget(self, index)
+                end
+            })
+        end
     end
+
+    do setupESX() end
 
     AddEventHandler("esx:jobsObjectRefreshed", function()
         if ESX?.Jobs then
@@ -78,5 +88,13 @@ else -- Only register these for the server
         if ESX?.Groups then
             ESX.Groups = ESX.GetGroups()
         end
+    end)
+
+    AddEventHandler("esx:sharedObjectUpdated", function()
+        ESX = exports["es_extended"]:getSharedObject()
+
+        do setupESX() end
+
+        collectgarbage("collect")
     end)
 end
