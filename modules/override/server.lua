9609b1fa95099cb35f141157df6c4bfa6a4c83dd
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-07-08 20:11:01 -0400

    tweak(server/classes/vehicle): setField & setMethod in override module

--- modules/override/server.lua
+++ modules/override/server.lua
@@ -195,4 +195,58 @@ do
             end
         end
     })
+
+    ESX.RegisterVehicleMethodOverrides({
+        ---Sets the value of the specified field for the xVehicle object. If a field with the same name already exist, its value will be overrided.
+        ---@param self xVehicle
+        setField = function(self)
+            ---@param fieldName string
+            ---@param value number | string | boolean | table
+            ---@return boolean (whether the registration action was successful or not)
+            return function(fieldName, value)
+                local fieldNameType = type(fieldName)
+                local valueType = type(value)
+                local isValueValid = (valueType == "number" or valueType == "string" or valueType == "boolean" or (valueType == "table" and not value?.__cfx_functionReference)) and true or false
+
+                if fieldNameType ~= "string" then print(("[^1ERROR^7] The field name (^3%s^7) passed in ^5vehicle(%s)'s setField()^7 is not a valid string!"):format(fieldName, self.entity)) return false
+                elseif not isValueValid then print(("[^1ERROR^7] The value passed in ^5vehicle(%s)'s setField()^7 does not have a valid type!"):format(self.entity)) return false end
+
+                if fieldName == "setField" or fieldName == "setMethod" then print(("[^1ERROR^7] Field ^2%s^7 of xVehicle ^1cannot^7 be overrided!"):format(fieldName)) return false end
+
+                self[fieldName] = value
+
+                if Config.EnableDebug then
+                    print(("[^5INFO^7] Setting field (^2%s^7) for vehicle(%s) through ^5xVehicle.setField()^7."):format(fieldName, self.entity))
+                end
+
+                return true
+            end
+        end,
+
+        ---Adds a new method/function to the current xVehicle object. If a method with the same name already exist, it will be overrided.
+        ---@param self xVehicle
+        setMethod = function(self)
+            ---@param fnName string
+            ---@param fn function
+            ---@return boolean (whether the registration action was successful or not)
+            return function(fnName, fn)
+                local fnNameType = type(fnName)
+                local fnType = type(fn)
+                local isFnValid = (fnType == "function" or (fnType == "table" and fn?.__cfx_functionReference and true)) or false
+
+                if fnNameType ~= "string" then print(("[^1ERROR^7] The method name (^3%s^7) passed in ^5vehicle(%s)'s setMethod()^7 is not a valid string!"):format(fnName, self.entity)) return false
+                elseif not isFnValid then print(("[^1ERROR^7] The function passed in ^5vehicle(%s)'s setMethod()^7 is not a valid function!"):format(self.entity)) return false end
+
+                if fnName == "setMethod" or fnName == "setField" then print(("[^1ERROR^7] Method ^2%s^7 of xVehicle ^1cannot^7 be overrided!"):format(fnName)) return false end
+
+                self[fnName] = fn(self)
+
+                if Config.EnableDebug then
+                    print(("[^5INFO^7] Setting method (^2%s^7) for vehicle(%s) through ^5xVehicle.setMethod()^7."):format(fnName, self.entity))
+                end
+
+                return true
+            end
+        end
+    })
 end
