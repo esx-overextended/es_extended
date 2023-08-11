aa60593a9fa408f57d35c50cba4855850dec8ec3
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-08-11 17:24:56 -0400

    feat(modules/integer): add ESX.GetRandomNumber

---
+++ modules/integer/shared.lua
@@ -0,0 +1,10 @@
+function ESX.GetRandomNumber(length)
+    if type(length) ~= "number" or length <= 0 then return end
+
+    math.randomseed(GetGameTimer())
+
+    local minRange = 10 ^ (length - 1)
+    local maxRange = (10 ^ length) - 1
+
+    return math.random(minRange, maxRange)
+end
