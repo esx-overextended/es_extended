d1e81195d47d44e02aea36b7d74b18d224afca46
BILLION <160111894+billiononly1@users.noreply.github.com>
2024-10-13 07:34:36 +0700

    Update server.lua
    -- Check if the user input is a valid "true" or "false"

--- modules/vehicleData/server.lua
+++ modules/vehicleData/server.lua
@@ -104,10 +104,20 @@ local function requestDataGenerationFromPlayer(playerId, args)
 end
 
 ESX.RegisterCommand("parsevehicles", "superadmin", function(xPlayer, args)
+    -- Convert "true/false" string to boolean
     local toBoolean = { ["false"] = false, ["true"] = true }
-    args.processAll = toBoolean[args.processAll:lower()] --[[@as boolean]]
 
-    requestDataGenerationFromPlayer(xPlayer.source, args)
+    -- Check if the user input is a valid "true" or "false"
+    if toBoolean[args.processAll:lower()] ~= nil then
+        -- Convert processAll argument to boolean
+        args.processAll = toBoolean[args.processAll:lower()]
+
+        -- Call requestDataGenerationFromPlayer function with converted argument
+        requestDataGenerationFromPlayer(xPlayer.source, args)
+    else
+        -- Notify the player if the input is invalid
+        xPlayer.showNotification("Invalid value for processAll. Please use true or false.")
+    end
 end, false, {
     help = "Generate and save vehicle data for available models on the client",
     validate = true,
@@ -115,6 +125,7 @@ end, false, {
         { name = "processAll", help = "true/false >> Whether the parsing process should include vehicles with existing data (in the event of updated vehicle stats)", type = "string" }
     }
 })
+
 
 ESX.RegisterCommand("parsevehicle", "superadmin", function(xPlayer, args)
     requestDataGenerationFromPlayer(xPlayer.source, args)
