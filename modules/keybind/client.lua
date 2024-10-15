a31d592d4cbf9d7b665274a68848345b49eba3ad
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2024-10-15 16:48:00 -0400

    tweak: better trace on key mapping

--- modules/keybind/client.lua
+++ modules/keybind/client.lua
@@ -11,25 +11,26 @@ end
 function ESX.RegisterInput(command_name, label, input_group, key, on_press, on_release)
     -- Check if the command is already registered
     if Core.Inputs[command_name] then
-        print(("Command '%s' is already registered. Skipping registration."):format(command_name))
-        return
+        return ESX.Trace(("Command '%s' is already registered! Skipping registration..."):format(command_name), "warning", true)
     end
 
     -- Convert the command name for press and release
     local is_release = on_release ~= nil
     local command_key = is_release and "+" .. command_name or command_name
 
+    local keyHash = ESX.HashString(command_key)
+
     -- Check if the key mapping is already registered
-    for cmd, _ in pairs(Core.Inputs) do
-        if Core.Inputs[cmd] == ESX.HashString(command_key) then
-            print(("Key mapping for '%s' is already registered. Skipping registration."):format(command_name))
-            return
+    for _, storedKeyHash in pairs(Core.Inputs) do
+        if storedKeyHash == keyHash then
+            return ESX.Trace(("Key mapping for the command '%s' is already registered! Skipping registration..."):format(command_name), "warning", true)
         end
     end
 
+    Core.Inputs[command_name] = keyHash
+
     -- Register the command for key press
     RegisterCommand(command_key, on_press, false)
-    Core.Inputs[command_name] = ESX.HashString(command_key)
 
     -- If on_release is provided, register the command for key release
     if is_release then
