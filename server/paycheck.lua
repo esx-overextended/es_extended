2b961b59cb4919b1a880cd5cad7d4bc54140772e
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2024-10-25 13:45:22 -0400

    fix(server/paycheck): properly handle off-duty salary

--- server/paycheck.lua
+++ server/paycheck.lua
@@ -21,35 +21,33 @@ function StartPayCheck()
                 local offduty_salary = xPlayer.job.grade_offduty_salary
                 local duty = xPlayer.job.duty
 
-                if salary > 0 then
-                    if duty then
-                        if not Config.EnableSocietyPayouts then
-                            ProcessPayCheck(xPlayer, salary, job == "unemployed" and _U("received_help", salary) or _U("received_salary", salary))
-                        else
-                            TriggerEvent("esx_society:getSociety", job, function(society)
-                                if society then
-                                    TriggerEvent("esx_addonaccount:getSharedAccount", society.account, function(account)
-                                        if account.money >= salary then
-                                            ProcessPayCheck(xPlayer, salary, _U("received_salary", salary))
-                                            account.removeMoney(salary)
-                                        else
-                                            xPlayer.triggerSafeEvent("esx:showAdvancedNotification", {
-                                                sender = _U("bank"),
-                                                subject = "",
-                                                message = _U("company_nomoney"),
-                                                textureDict = "CHAR_BANK_MAZE",
-                                                iconType = 1,
-                                            })
-                                        end
-                                    end)
-                                else
-                                    ProcessPayCheck(xPlayer, salary, job == "unemployed" and _U("received_help", salary) or _U("received_salary", salary))
-                                end
-                            end)
-                        end
-                    elseif offduty_salary > 0 then
-                        ProcessPayCheck(xPlayer, offduty_salary, job == "unemployed" and _U("received_help", offduty_salary) or _U("received_salary", offduty_salary))
+                if duty and salary > 0 then
+                    if not Config.EnableSocietyPayouts then
+                        ProcessPayCheck(xPlayer, salary, job == "unemployed" and _U("received_help", salary) or _U("received_salary", salary))
+                    else
+                        TriggerEvent("esx_society:getSociety", job, function(society)
+                            if society then
+                                TriggerEvent("esx_addonaccount:getSharedAccount", society.account, function(account)
+                                    if account.money >= salary then
+                                        ProcessPayCheck(xPlayer, salary, _U("received_salary", salary))
+                                        account.removeMoney(salary)
+                                    else
+                                        xPlayer.triggerSafeEvent("esx:showAdvancedNotification", {
+                                            sender = _U("bank"),
+                                            subject = "",
+                                            message = _U("company_nomoney"),
+                                            textureDict = "CHAR_BANK_MAZE",
+                                            iconType = 1,
+                                        })
+                                    end
+                                end)
+                            else
+                                ProcessPayCheck(xPlayer, salary, job == "unemployed" and _U("received_help", salary) or _U("received_salary", salary))
+                            end
+                        end)
                     end
+                elseif not duty and offduty_salary > 0 then
+                    ProcessPayCheck(xPlayer, offduty_salary, job == "unemployed" and _U("received_help", offduty_salary) or _U("received_salary", offduty_salary))
                 end
             end
         end
